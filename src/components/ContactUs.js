b7fa09403f1f28124f2d43f5d3488f1f9f6d30bb
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-09-12 18:26:10 +0530

    ...form

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,7 +1,59 @@
 import React from "react";
 
 const ContactUs = () => {
-  return <div className="contact-container">ContactUs</div>;
+  const handleSubmit = () => {};
+
+  return (
+    <div className="contact-container">
+      <div></div>
+      <div>
+        <form className="form" onSubmit={handleSubmit}>
+          <label id="name">
+            Name :
+            <br />
+            <input
+              className="form-input"
+              id="name"
+              placeholder="Enter your name"
+            />
+          </label>
+          <br />
+          <label id="email">
+            Email :
+            <br />
+            <input
+              className="form-input"
+              id="email"
+              placeholder="Enter your email"
+            />
+          </label>
+          <br />
+          <label id="no">
+            Mobile No. :
+            <br />
+            <input
+              className="form-input"
+              id="no"
+              placeholder="Enter your mobile number"
+            />
+          </label>
+          <br />
+          <label id="message">
+            Your Message :
+            <br />
+            <textarea
+              className=""
+              id="message"
+              placeholder="Enter your message"
+            />
+          </label>
+          <br />
+          <button className="btn">Submit</button>
+          <h1>.............in Progress.. Available soon. . .</h1>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
