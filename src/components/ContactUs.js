53b784eb5086de6b02f1a75c97568c29d19731e8
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-09-13 11:50:22 +0530

    contact-us updated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,57 +1,61 @@
 import React from "react";
+import { Button, Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 
 const ContactUs = () => {
   const handleSubmit = () => {};
 
   return (
     <div className="contact-container">
-      <div></div>
-      <div>
-        <form className="form" onSubmit={handleSubmit}>
-          <label id="name">
-            Name :
-            <br />
-            <input
-              className="form-input"
-              id="name"
-              placeholder="Enter your name"
-            />
-          </label>
-          <br />
-          <label id="email">
-            Email :
-            <br />
-            <input
-              className="form-input"
-              id="email"
-              placeholder="Enter your email"
-            />
-          </label>
-          <br />
-          <label id="no">
-            Mobile No. :
-            <br />
-            <input
-              className="form-input"
-              id="no"
-              placeholder="Enter your mobile number"
-            />
-          </label>
-          <br />
-          <label id="message">
-            Your Message :
-            <br />
-            <textarea
-              className=""
-              id="message"
-              placeholder="Enter your message"
-            />
-          </label>
-          <br />
-          <button className="btn">Submit</button>
-          <h1>.............in Progress.. Available soon. . .</h1>
-        </form>
-      </div>
+      <Container fluid="md">
+        <Row>
+          <Col md={5} className="p-5">
+            <p className="text-center">Get in touch</p>
+            <h2 className="text-center">Available Soon. . .</h2>
+          </Col>
+          <Col md={{ span: 5, offset: 2 }} className="p-5">
+            <p className="text-center">Contact me</p>
+
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>Your Name</Form.Label>
+
+                <Form.Control type="text" placeholder="Enter Name" />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Mobile number</Form.Label>
+                <Form.Control type="number" placeholder="Enter mobile number" />
+                <Form.Text className="text-muted">
+                  We'll never share your number with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Your Message</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="text"
+                  as="textarea"
+                  rows={6}
+                  placeholder="Enter your message"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
