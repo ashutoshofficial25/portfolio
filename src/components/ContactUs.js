80ddf88f91a5f0929e0322cae977b656ee7c16c9
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-09-13 13:21:26 +0530

    form validation

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 
 const ContactUs = () => {
-  const handleSubmit = () => {};
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
 
   return (
     <div id="contact-me" className="contact-container">
@@ -15,27 +25,42 @@ const ContactUs = () => {
           <Col md={{ span: 5, offset: 2 }} className="p-5">
             <p className="text-center">Contact me</p>
 
-            <Form onSubmit={handleSubmit}>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label>Your Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Name" required />
 
-                <Form.Control type="text" placeholder="Enter Name" />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your name
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" required />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
+
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid email
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <Form.Label>Mobile number</Form.Label>
-                <Form.Control type="number" placeholder="Enter mobile number" />
+                <Form.Control
+                  type="number"
+                  placeholder="Enter mobile number"
+                  required
+                />
                 <Form.Text className="text-muted">
                   We'll never share your number with anyone else.
                 </Form.Text>
+
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid mobile number
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group className="mb-3">
@@ -46,7 +71,11 @@ const ContactUs = () => {
                   as="textarea"
                   rows={6}
                   placeholder="Enter your message"
+                  required
                 />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a a message to understand your requirement!
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Button variant="primary" type="submit">
