793a844b72b34cfdeaa9663c3d2bcf2b2f2d6bcc
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-09-14 11:51:02 +0530

    projects updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import estyIndia from "../assets/projects/esty-india.png";
+import visitNewLocation from "../assets/projects/visit-new-location.png";
+import todoApp from "../assets/projects/todo-app.png";
 
 const Projects = () => {
   const cardData = [
@@ -15,38 +18,41 @@ const Projects = () => {
     <div className="project-section bg-secondary" id="projects">
       <Container fluid>
         <h1 className="text-center">Projects</h1>
-        <h2 className="text-center">Working on This section</h2>
 
-        <Row className="justify-content-center">
-          <Col lg={4} xl={4} md={6} sm={6} xs={12} className="mx-auto">
+        <Row className="justify-content-center p-5">
+          <Col lg={4} xl={4} md={6} sm={6} xs={12} className="project-col  p-3">
             <h4 className="projectTitle">
-              Project Name <span>some. some.</span>
+              Easy India <span>1st page</span>
             </h4>
-            <a href="#" target="_blank">
+            <a href="https://esty-india.netlify.app/" target="_blank">
               <div className="cardProject">
-                <img src="#" className="d-block w-100" alt="..." />
+                <img src={estyIndia} className="d-block w-100" alt="..." />
               </div>
             </a>
           </Col>
 
-          <Col lg={4} xl={4} md={6} sm={6} xs={12}>
+          <Col lg={4} xl={4} md={6} sm={6} xs={12} className="project-col  p-3">
             <h4 className="projectTitle">
-              Project Name <span>some. some.</span>
+              Visit new location <span>(Natours-clone)</span>
             </h4>
-            <a href="#" target="_blank">
+            <a href="https://visitnewlocation.netlify.app/" target="_blank">
               <div className="cardProject">
-                <img src="#" className="d-block w-100" alt="..." />
+                <img
+                  src={visitNewLocation}
+                  className="d-block w-100"
+                  alt="..."
+                />
               </div>
             </a>
           </Col>
 
-          <Col lg={4} xl={4} md={6} sm={6} xs={12}>
+          <Col lg={4} xl={4} md={6} sm={6} xs={12} className="project-col  p-3">
             <h4 className="projectTitle">
               Project Name <span>some. some.</span>
             </h4>
-            <a href="#" target="_blank">
+            <a href="https://todowebapp.vercel.app/" target="_blank">
               <div className="cardProject">
-                <img src="#" className="d-block w-100" alt="..." />
+                <img src={todoApp} className="d-block w-100" alt="..." />
               </div>
             </a>
           </Col>
