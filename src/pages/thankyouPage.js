736ce6b825c796ff2eabd618d9a06fb895c9b6ee
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-09-21 13:23:36 +0530

    thankyou page updated

--- src/pages/thankyouPage.js
+++ src/pages/thankyouPage.js
@@ -4,6 +4,9 @@ import React from "react";
 import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
+//Asset Imports
+import thanyouImg from "../assets/thankyou.svg";
+
 const ThankyouPage = () => {
   const navigate = useNavigate();
   const goBack = () => {
@@ -11,6 +14,7 @@ const ThankyouPage = () => {
   };
   return (
     <Container className="thankyou-page text-white" fluid>
+      <img src={thanyouImg} />
       <h1> Thankyou !</h1>
       <p>My team will get you soon!..</p>
 
