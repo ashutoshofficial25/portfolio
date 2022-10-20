023880d4b15975ca957e45675d7f4a4a1744b6fc
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-10-20 16:32:54 +0530

    service image updates

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,8 @@ import { motion } from "framer-motion";
 
 //Asset Imports
 import webImg from "../assets/services/web.svg";
-
+import design from "../assets/services/service-2.svg";
+import responsive from "../assets/services/service-3.svg";
 const Services = () => {
   return (
     <div id="services" className="our-services">
@@ -34,7 +35,7 @@ const Services = () => {
           whileHover={{ scale: 1.1 }}
           transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
-          <img className="img-1" src={webImg} alt="web-app" />
+          <img className="img-1" src={design} alt="web-app" />
 
           <div className="card-head text-center">
             Website Design/Development
@@ -53,7 +54,7 @@ const Services = () => {
           whileHover={{ scale: 1.1 }}
           transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
-          <img className="img-1" src={webImg} alt="web-app" />
+          <img className="img-1" src={responsive} alt="web-app" />
 
           <div className="card-head">Responsive Design</div>
           <p className="card-text">
