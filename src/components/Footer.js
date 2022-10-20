b0afd0b7585518e221a3cb7ba39f2f91cc1ab253
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-10-20 16:04:36 +0530

    footer-updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <p className="footer-text">
-        &#169; 2022 copywrites Ashutosh Maurya | All right reserved. Terms &
-        conditions.
+      <p className="footer-text text-dark">
+        Developed and desined by Ashutosh Maurya
+      </p>
+      <hr />
+      <p className="footer-text text-dark">
+        &#169; 2022 copywrites ashutosh-maurya.netlify.app | All right reserved.
       </p>
     </div>
   );
